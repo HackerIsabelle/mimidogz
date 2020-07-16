1a238b7b0e520ad699cc51f4373705a492ad42dd
IsabelleOnCyber <68364998+HackerIsabelle@users.noreply.github.com>
2020-07-15 23:44:12 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
    skebeedebeepbop

--- main.cpp
+++ main.cpp
@@ -13,11 +13,11 @@ int shellSort(int arr[], int n)
         // The first gap elements a[0..gap-1] are already in gapped order 
         // keep adding one more element until the entire array is 
         // gap sorted  
-        for (int i = gap; i < n; i += 1) 
+        for (int i = gap; i < n; i += 2) 
         { 
             // add a[i] to the elements that have been gap sorted 
             // save a[i] in temp and make a hole at position i 
-            int temp = arr[i]; 
+            int temp = arr[i - 1]; 
   
             // shift earlier gap-sorted elements up until the correct  
             // location for a[i] is found 
